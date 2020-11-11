9e068dbce78eb0731751618efc300bd59b1d82b2
Jan Maxime <maxime.jan@epfl.ch>
2020-11-12 00:40:41 +0100

    Fix tags

--- website/search.py
+++ website/search.py
@@ -1,4 +1,7 @@
 import pandas as pd
 
-def search_item_in_database(item, collection):
-    return list(collection.find( {"obj_boxes." + item : {"$exists" : True}}))
+def search_bb_and_tags(item, collection):
+    return list(collection.find({"$or" : [{"obj_boxes." + item : {"$exists" : True}}, {"tags" : item }]}))
+
+def search_bb(item, collection):
+    return list(collection.find({"obj_boxes." + item : {"$exists" : True}}))
