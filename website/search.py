cd30404e121d3f7594735c266f1c2162d15ada9f
Jan Maxime <maxime.jan@epfl.ch>
2020-11-18 23:31:26 +0100

    Add gallery

--- website/search.py
+++ website/search.py
@@ -5,3 +5,7 @@ def search_bb_and_tags(item, collection):
 
 def search_bb(item, collection):
     return list(collection.find({"obj_boxes." + item : {"$exists" : True}}))
+
+
+def search_country(country, collection):
+    return list(collection.find({ "annotation.country" : country}))
