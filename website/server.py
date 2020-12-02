c343a2c31ddaaf197ac8f2435174e36e5ddc0847
Jan Maxime <maxime.jan@epfl.ch>
2020-12-02 20:36:48 +0100

    Add pagination on gallery

--- website/server.py
+++ website/server.py
@@ -19,6 +19,8 @@ UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = set(['png', 'jpeg', 'jpg'])
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+PAGE_SIZE = 21
+
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -33,19 +35,19 @@ def home():
 @app.route("/gallery", methods=["GET", "POST"])
 def gallery():
     iiifs_and_links = []
+
     if request.method == "POST":
         display_markers = request.form.get("display_markers", False)
         if display_markers:
             results = get_markers(sample_annotations)
-            lat_lng_name = [result["annotation"]["landmark_info"]
-                            for result in results]
+            lat_lng_name = [result["annotation"]["landmark_info"] for result in results]
             return jsonify({"lat_lng_name": lat_lng_name})
         else:
-            results = search_country(
-                request.form["country"], sample_annotations)
+            page_number = int(request.form["page_number"])
+            results = search_country(request.form["country"], sample_annotations, page_number, PAGE_SIZE)
             iiifs_and_links = [(json.dumps(result["annotation"]["iiif"]), result["annotation"]["iiif"]["images"]
-                                [0]["resource"]["service"]["@id"][:-4] + "/full/,360/0/default.jpg") for result in results]
-            return jsonify({"data": render_template("display_images.html", iiifs_and_links=iiifs_and_links)})
+                                [0]["resource"]["service"]["@id"][:-4] + "/square/360,/0/default.jpg") for result in results]
+            return jsonify({"data": render_template("display_images.html", iiifs_and_links=iiifs_and_links, page_size = PAGE_SIZE, page_number = page_number)})
 
     return render_template("gallery.html")
 
@@ -54,21 +56,23 @@ def gallery():
 def search_page():
     iiif_and_links = []
     number_of_results = 0
+    page_number = 0
     display_bb = False
     item = ""
     if request.method == "POST":
         if request.form["submit"] == "text_search":
+            page_number = int(request.form["page_number"])
+            print(page_number)
             item = request.form["item"]
             display_bb = request.form.get("display_bb", False)
-            photos, searched_items = search_photos(
-                item, sample_tags, sample_annotations)
+            photos, searched_items, number_of_results = search_photos(
+                item, sample_tags, sample_annotations, page_number, PAGE_SIZE)
 
             if not display_bb:
                 for res_photo in photos:
                     photo = res_photo["annotation"]
                     new_item = (json.dumps(photo["iiif"]), [
-                                photo["iiif"]["images"][0]["resource"]["service"]["@id"][:-4] + "/full/,360/0/default.jpg"])
-                    number_of_results += len(new_item[1])
+                                photo["iiif"]["images"][0]["resource"]["service"]["@id"][:-4] + "/square/360,/0/default.jpg"])
                     iiif_and_links.append(new_item)
             else:
                 for res_photo in photos:
@@ -78,8 +82,7 @@ def search_page():
                             (key for key in photo["obj_boxes"] if sitem in key), None)
                         if box_key:
                             new_item = (json.dumps(photo["iiif"]), [photo["iiif"]["images"][0]["resource"]["service"]["@id"][:-4] + "/" + str(
-                                box[0]) + "," + str(box[1]) + "," + str(box[2]) + "," + str(box[3]) + "/max/0/default.jpg" for box in photo["obj_boxes"][box_key]])
-                            number_of_results += len(new_item[1])
+                                box[0]) + "," + str(box[1]) + "," + str(box[2]) + "," + str(box[3]) + "/360,360/0/default.jpg" for box in photo["obj_boxes"][box_key]])
                             iiif_and_links.append(new_item)
         elif request.form["submit"] == "image_search":
 
@@ -88,16 +91,15 @@ def search_page():
             else:
                 image_file = request.files["image_file"]
                 if image_file and allowed_file(image_file.filename):
-                    similar_photos = search_similar_photos(
-                        image_file, sample_imageVectors, sample_annotations, count=20)
+                    similar_photos = search_similar_photos(image_file, sample_imageVectors, sample_annotations, count=20)
                     for res_photo in similar_photos:
                         photo = res_photo["annotation"]
                         new_item = (json.dumps(photo["iiif"]), [
-                            photo["iiif"]["images"][0]["resource"]["service"]["@id"][:-4] + "/full/,360/0/default.jpg"])
+                            photo["iiif"]["images"][0]["resource"]["service"]["@id"][:-4] + "/square/360,/0/default.jpg"])
                         number_of_results += len(new_item[1])
                         iiif_and_links.append(new_item)
 
-    return render_template("search.html", results=iiif_and_links, number_of_results=number_of_results, display_bb=display_bb, item=item, cold_start=request.method == "GET")
+    return render_template("search.html", results=iiif_and_links, number_of_results=number_of_results, display_bb=display_bb, item=item, cold_start=request.method == "GET", page_size = PAGE_SIZE, page_number = page_number)
 
 
 if __name__ == "__main__":
