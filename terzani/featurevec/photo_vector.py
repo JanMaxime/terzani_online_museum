1435a5a66c77ec798f3db31cb78782d6f707c669
Ravinithesh Reddy Annapureddy <ravinitheshreddy@gmail.com>
2020-12-06 18:49:52 +0100

    created a module to get image feature vector

---
+++ terzani/featurevec/photo_vector.py
@@ -0,0 +1,75 @@
+from purl import URL
+from PIL import Image
+from io import BytesIO
+import requests
+import matplotlib.pyplot as plt
+from torch.autograd import Variable
+import torchvision.transforms as transforms
+import torchvision.models as models
+import torch
+
+
+def load_resnet_model():
+    """
+    This function return a pretrained resnet model, the layer at which the feature vector needs to be extracted
+    and the transformations to be performed on the image.
+
+    :return model: The pretrained Resnet Model
+    :return layer: The averaging pool layer of the Resnet 18 model
+    :return scaler: Transformer to resize the image to fit into rest
+    :return normalize: Transformer to normalise the image
+    :return to_tensor: Transformer to convert the image into Tensor
+    """
+
+    # Loading pretrained Resnet model
+    model = models.resnet18(pretrained=True)
+    layer = model._modules.get('avgpool')
+    # Set model to evaluation mode as we plan to use pre trained model
+    model.eval()
+    scaler = transforms.Resize((224, 224))
+    normalize = transforms.Normalize(
+        mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    to_tensor = transforms.ToTensor()
+
+    return model, layer, scaler, normalize, to_tensor
+
+
+def transform_image(image, scaler, normalize, to_tensor):
+    """
+    This function transforms the image to be suitable for processing by a pretrained resnet model.
+
+    :param image: The image to be transformed
+    :param scaler: Transformer to resize the image to fit into rest
+    :param normalize: Transformer to normalise the image
+    :param to_tensor: TTransformer to convert the image into Tensor
+    :return : Transformed image
+    """
+    return Variable(normalize(to_tensor(scaler(image))).unsqueeze(0))
+
+
+def get_vector(image_url: URL, model, layer, scaler, normalize, to_tensor):
+    """
+    This function accepts the image URL, model, transformations and returns the feature vector of the image.
+
+    :param image_url: The url of the image
+    :param model: The pretrained Resnet Model
+    :param layer: The averaging pool layer of the Resnet 18 model
+    :param scaler: Transformer to resize the image to fit into rest
+    :param normalize: Transformer to normalise the image
+    :param to_tensor: TTransformer to convert the image into Tensor
+    :return my_embedding(tensor): The feature vector of the image obtained through resnet 18
+    """
+
+    response = requests.get(image_url)
+    img = Image.open(BytesIO(response.content))
+    t_img = transform_image(img, scaler, normalize, to_tensor)
+    #  The avgpool layer that we selected has an output size of 512
+    my_embedding = torch.zeros(512)
+
+    def copy_data(m, i, o):
+        my_embedding.copy_(o.data.squeeze())
+
+    h = layer.register_forward_hook(copy_data)
+    model(t_img)
+    h.remove()
+    return my_embedding
