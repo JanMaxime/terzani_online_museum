271b5f13b9eaa75480538bbffdd4d40295327d67
Ravinithesh Reddy Annapureddy <ravinitheshreddy@gmail.com>
2020-12-06 18:51:13 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="terzani",
+    version="0.0.1",
+    author="Ravinithesh",
+    author_email="ravinithesh.annapureddy@epfl.ch",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.8',
+)
