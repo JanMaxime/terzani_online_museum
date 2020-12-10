b1d6e2cf3a15250baf8c19c272dca02c4784e16b
Ravinithesh Reddy Annapureddy <ravinitheshreddy@gmail.com>
2020-12-11 00:57:30 +0100

    modified fucntions to be useful for search and db

--- terzani/featurevec/photo_vector.py
+++ terzani/featurevec/photo_vector.py
@@ -7,6 +7,17 @@ from torch.autograd import Variable
 import torchvision.transforms as transforms
 import torchvision.models as models
 import torch
+from typing import Union
+
+
+def get_similarity(img_vec1, img_vec2):
+    if not torch.is_tensor(img_vec1):
+        img_vec1 = torch.Tensor(img_vec1)
+    if not torch.is_tensor(img_vec2):
+        img_vec2 = torch.Tensor(img_vec2)
+    cossim = torch.nn.CosineSimilarity(dim=1, eps=1e-6)
+    sim = cossim(img_vec1.unsqueeze(0), img_vec2.unsqueeze(0))
+    return sim
 
 
 def load_resnet_model():
@@ -47,21 +58,25 @@ def transform_image(image, scaler, normalize, to_tensor):
     return Variable(normalize(to_tensor(scaler(image))).unsqueeze(0))
 
 
-def get_vector(image_url: URL, model, layer, scaler, normalize, to_tensor):
+def get_vector(image: Union[URL], model, layer, scaler, normalize, to_tensor, is_url=True):
     """
     This function accepts the image URL, model, transformations and returns the feature vector of the image.
 
-    :param image_url: The url of the image
+    :param image: The image or the url of the image
     :param model: The pretrained Resnet Model
     :param layer: The averaging pool layer of the Resnet 18 model
     :param scaler: Transformer to resize the image to fit into rest
     :param normalize: Transformer to normalise the image
-    :param to_tensor: TTransformer to convert the image into Tensor
+    :param to_tensor: Transformer to convert the image into Tensor
+    :param is_url: To indicate if the input image is a Image file or a URL
     :return my_embedding(tensor): The feature vector of the image obtained through resnet 18
     """
 
-    response = requests.get(image_url)
-    img = Image.open(BytesIO(response.content))
+    if is_url:
+        response = requests.get(image)
+        img = Image.open(BytesIO(response.content))
+    else:
+        img = Image.open(image).convert('RGB')
     t_img = transform_image(img, scaler, normalize, to_tensor)
     #  The avgpool layer that we selected has an output size of 512
     my_embedding = torch.zeros(512)
