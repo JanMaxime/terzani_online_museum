b82bcced24d7893126f15d41cbaa831873b5c86a
Ravinithesh Reddy Annapureddy <ravinitheshreddy@gmail.com>
2020-12-10 12:03:54 +0100

    Update search.py

--- website/search.py
+++ website/search.py
@@ -1,55 +1,29 @@
 from imageRetrieval import get_imagevector, get_similarity
-import string
+from terzani.utils.clean_text import clean_text
 from itertools import chain, islice
-import nltk
-from nltk.tokenize import word_tokenize
-from nltk.stem.porter import PorterStemmer
-nltk.download('punkt')
 
 
-def clean_text(text: str, lower: bool = True, rmv_punc: bool = True, stem: bool = True, norm: bool = True):
-    """
-    This function accepts a string and performs preprocessing steps on it. 
+def search_photos(item, tag_collection, annotation_collection, page_number, page_size, min_len=3, exact_match=False):
 
-    :param text (str): The string or text on which the preprocessing has to be performed.
-    :param lower (bool): Default=True, indicates if the text has to be made into lower case.
-    :param rmv_punc (bool): Default=True, indicates if the punctuation should be removed in the text.
-    :param stem (bool): Default=True, indicates if the stemming should be performed on the words in the text.
-    :param norm (bool): Default=True, indicates if the words in the has to be normalised.
-    :return cleaned_text (list): The modified text is returned as list after performing the indicated operations.
-    """
-
-    # split into words
-    tokens = word_tokenize(text)
-    if lower:
-        # convert to lower case
-        tokens = [w.lower() for w in tokens]
-    if rmv_punc:
-        # remove punctuation from each word
-        table = str.maketrans('', '', string.punctuation)
-        tokens = [w.translate(table)
-                  for w in tokens if w.translate(table) != '']
-    if stem:
-        # stemming of words
-        porter = PorterStemmer()
-        tokens = [porter.stem(word) for word in tokens]
-    cleaned_text = sorted(list(set(tokens)), key=str.lower)
-    return cleaned_text
-
-
-def search_photos(item, tag_collection, annotation_collection, page_number, page_size, min_len=3):
-    clean_query = clean_text(item)
+    if not exact_match:
+        clean_query = clean_text(item)
+    else:
+        clean_query = clean_text(item, stem=False)
     clean_query_ocr = clean_text(
-        item, lower=True, rmv_punc=False, stem=False, norm=False)
+        item, lower=True, rmv_punc=False, stem=False)
     search_items = list(set(clean_query + clean_query_ocr))
     selected_photos = []
     for query in search_items:
         if len(query) >= min_len:
+            if not exact_match:
+                search_query = "(.*)"+query+"(.*)"
+            else:
+                search_query = query
             for query_photos in tag_collection.aggregate([
                 {
                     "$search": {
                         "regex": {
-                            "query": "(.*)"+query+"(.*)",
+                            "query": search_query,
                             "path": "tag",
                             "allowAnalyzedField": True,
                         }
@@ -65,20 +39,20 @@ def search_photos(item, tag_collection, annotation_collection, page_number, page
             ]
             ):
                 selected_photos.extend([query_photos])
-        sorted_selected_photos = sorted(
-            selected_photos, key=lambda photo: photo["score"], reverse=True)
-        photo_sorted = [res_dict['images']
-                        for res_dict in sorted_selected_photos if 'images' in res_dict]
-        photo_sorted_flat = list(chain.from_iterable(photo_sorted))
-        photo_results = list(set(photo_sorted_flat))
-        n_total_results = len(photo_results)
-        photo_results = photo_results[page_number *
-                                      page_size: page_number * page_size + page_size]
-        full_images = list()
-        for res_image in photo_results:
-            image_iiif = list(annotation_collection.find({"label": res_image}))
-            if image_iiif:
-                full_images.append(image_iiif[0])
+    sorted_selected_photos = sorted(
+        selected_photos, key=lambda photo: photo["score"], reverse=True)
+    photo_sorted = [res_dict['images']
+                    for res_dict in sorted_selected_photos if 'images' in res_dict]
+    photo_sorted_flat = list(chain.from_iterable(photo_sorted))
+    photo_results = list(set(photo_sorted_flat))
+    n_total_results = len(photo_results)
+    photo_results = photo_results[page_number *
+                                  page_size: page_number * page_size + page_size]
+    full_images = list()
+    for res_image in photo_results:
+        image_iiif = list(annotation_collection.find({"label": res_image}))
+        if image_iiif:
+            full_images.append(image_iiif[0])
     return full_images, search_items, n_total_results
 
 
